a3a84ab4bb34fa710af18e057cbf06e2bf58260b
waiwaitaro <sekutonia@icloud.com>
2025-01-31 23:22:58 +0900

    first_commit

---
+++ java/Greeting.java
@@ -0,0 +1,11 @@
+package java;
+class Greeting{
+  public static void main(String args[]){
+    System.out.println("Good afternoon");
+    /*system.out.println()は標準出力へ引数に指定した値を出力する */
+
+    System.out.println("Good morning");
+    
+    System.out.println("Good evening");
+  }
+}
---
+++ java/Lesson06_01.java
@@ -0,0 +1,20 @@
+package java;
+class Lesson06_01 {
+  public static void main(String[] args) {
+    // int var-01 = 1;
+    // int 01var = 1;
+
+    // System.out.println(var-01);
+
+    int var = 1;
+    int var01 = 2;
+    int var_01 = 3;
+    int var$01 = 4;
+
+    System.out.println(var);
+    System.out.println(var01);
+    System.out.println(var_01);
+    System.out.println(var$01);
+
+  }
+}
